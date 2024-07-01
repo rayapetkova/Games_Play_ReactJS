359a809b7d4e6cb332ff342a23576dd3adf4c8cb
Raya Petkova <122923696+rayapetkova@users.noreply.github.com>
2024-07-02 01:01:45 +0300

    Comments functionality - authorization and requests (it needs a rework)

--- client/src/services/commentServices.js
+++ client/src/services/commentServices.js
@@ -1,11 +1,18 @@
-const baseUrl = 'http://localhost:3030/jsonstore'
+const baseUrl = 'http://localhost:3030/data'
 
 export async function createComment(data) {
+    let headers = {
+        'content-type': 'application/json'
+    }
+
+    const token = localStorage.getItem('accessToken')
+    if (token) {
+        headers['X-Authorization'] = token
+    }
+
     let response = await fetch(`${baseUrl}/comments`,{
         method: 'POST',
-        headers: {
-            'content-type': 'application/json'
-        },
+        headers: headers,
         body: JSON.stringify(data)
     })
 
@@ -14,7 +21,7 @@ export async function createComment(data) {
 }
 
 export async function getComments() {
-    let response = await fetch('http://localhost:3030/jsonstore/comments')
+    let response = await fetch(`${baseUrl}/comments`)
     let result = await response.json()
 
     return Object.values(result)
